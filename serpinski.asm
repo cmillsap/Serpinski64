0cfb46bd8e6b9e0751226ec6b8e3c4449444f7f5
cmillsap <christopher.millsap@gmail.com>
2019-06-16 16:32:06 -0500

    Commodore 64 ASM implementation of Serpinski Gasket
    Works fairly well, but may have a bug on the left side.

---
+++ serpinski.asm
@@ -0,0 +1,186 @@
+*=$0801
+        .byte $0E, $08, $0A, $00, $9E, $20, $28, $32
+        .byte $30, $36, $34, $29, $00, $00, $00     // sys (2064)
+
+.label scratchPad = $fd
+.label GFX_MEM = $2000 
+
+init:     lda #$00 
+          sta CurrPoint
+          sta CurrPoint+1 
+          sta CurrentX+1 
+          sta CurrentY+1 
+          lda #159 
+          sta CurrentX
+          lda #99
+          sta CurrentY
+biton:    lda $d018 
+          ora #$08 
+          sta $d018 // set bitmap location to $2000 
+          lda $d011 
+          ora #$20 
+          sta $d011 // turn on bitmap graphics mode 
+          lda #>GFX_MEM
+          tax
+          sta scratchPad+1 
+          lda #<GFX_MEM
+          sta scratchPad 
+ cls:     ldy #$ff 
+ clinner:  sta (scratchPad),y // clear the bitmap 
+           dey 
+           bne clinner 
+           inx 
+           stx scratchPad+1 
+           cpx #$40 
+           bne cls
+clcset:    lda #$04   //clear color RAM at $0400 - be careful near the end or BASIC memory will be corrupted
+           sta scratchPad+1
+           tax 
+           lda #$10 
+ clcolor:  ldy #$ff 
+ clcinner: sta (scratchPad),y 
+           dey
+           bne clcinner 
+           inx 
+           stx scratchPad+1 
+           cpx #$07
+           bne clcolor
+           ldy #$e8 
+clclast:   sta (scratchPad),y 
+           dey 
+           bne clclast
+mainLoop:  clc 
+           ldx #$00 
+           jsr random
+           cmp #85  
+           bcc cont
+           inx
+           inx 
+           cmp #170 
+           bcc cont 
+           inx
+           inx  
+  cont:    sec
+           lda VertexY,x
+           sbc CurrentY
+           sta DeltaY 
+           lda VertexY+1,x 
+           sbc CurrentY+1 
+           sta DeltaY+1 
+           sec 
+           lda VertexX,x 
+           sbc CurrentX 
+           sta DeltaX 
+           lda VertexX+1,x 
+           sbc CurrentX+1 
+           sta DeltaX+1 
+           clc 
+           lda DeltaX+1 
+           asl 
+           ror DeltaX+1 
+           ror DeltaX 
+           clc 
+           lda DeltaY+1 
+           asl  
+           ror DeltaY+1 
+           ror DeltaY 
+           clc 
+           lda CurrentX  
+           adc DeltaX 
+           sta CurrentX
+           lda CurrentX+1
+           adc DeltaX+1  
+           sta CurrentX+1 
+           clc 
+           lda CurrentY
+           adc DeltaY 
+           sta CurrentY
+           lda CurrentY+1 
+           adc DeltaY+1 
+           sta CurrentY+1
+           jsr plot 
+           clc 
+           inc CurrPoint 
+           beq currPntHi
+           jmp mainLoop 
+ currPntHi: inc CurrPoint+1 
+           lda CurrPoint+1 
+           cmp #$10
+           beq waitSpace 
+           jmp mainLoop 
+ waitSpace:lda #$7F   // found in lemon64 forums
+           sta $DC00 
+           lda $DC01 
+           and #$10  
+           bne waitSpace  
+           jsr $FF5B   //CINT - set machine back to defaults.   
+           rts 
+      
+plot: stx pllX+1 // Based on plot routine from Codebase64
+      sty pllY+1  
+      clc
+      ldx CurrentX
+      ldy CurrentY 
+      lda CurrentX+1 
+      beq noHY
+      sec 
+ noHY:lda YTableHi,y 
+      bcc !+
+      adc #$00      // Adds 1 (256 pixels) to HiByte
+!:    sta $fc
+      lda YTableLo,y
+      sta $fb
+      ldy XTable,x
+      lda BitMask,x
+      ora ($fb),y
+      sta ($fb),y
+pllX: ldx #$00 
+pllY: ldy #$00 
+      rts
+
+random: stx randx+1 //lfsr from CodeBase64
+        clc
+        ldx CurrPoint+1 
+        lda seed   
+        beq doEor
+        asl
+        beq noEor //if the input was $80, skip the EOR
+        bcc noEor
+doEor:  eor randomEor,x
+noEor:  sta seed
+randx:  ldx #$00  
+        rts 
+
+VertexX: .word 0 
+         .word 319 
+         .word 159
+VertexY: .word 0
+         .word 0 
+         .word 199 
+CurrentX: .word  159
+CurrentY: .word 99 
+DeltaX: .word 0 
+DeltaY: .word 0
+seed:  .byte 78 
+CurrPoint: .word 0 
+errorPoint: .byte 0 
+randomEor:  .byte $1d, $2b, $2d, $4d, $5f, $63, $65, $69
+            .byte $71, $87, $8d, $a9, $c3, $cf, $e7, $f5
+
+
+         
+
+ //generate tables for bitmap access x and y lookup 
+     .align $100
+BitMask:
+    .fill 256, pow(2,7-i&7)
+    .align $100
+XTable:
+    .fill 256, floor(i/8)*8
+    .align $100
+YTableHi:
+    .fill 200, >GFX_MEM+[320*floor(i/8)]+[i&7]
+    .align $100
+YTableLo:
+    .fill 200, <GFX_MEM+[320*floor(i/8)]+[i&7]
+

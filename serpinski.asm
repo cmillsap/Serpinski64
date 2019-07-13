28fc9fb3d90ba48c5bb46dc1df94bd4330a43c4b
cmillsap <christopher.millsap@gmail.com>
2019-07-13 11:46:57 -0500

    Speed Inprovements and Bug Fixes

--- serpinski.asm
+++ serpinski.asm
@@ -4,18 +4,22 @@
 
 .label scratchPad = $fd
 .label GFX_MEM = $2000 
+.label borderColor = $D0
 .label currXStart = 159 
 .label currYStart = 99
+.label DeltaX = $fb 
+.label DeltaY = $fd
+.label BorColor = $0d
+.label memColor = $d0
 
-init:     lda #$00 
+init:     getTime(time1)
+          lda #BorColor
+          sta $d020 
+          lda #$00 
           sta CurrPoint
           sta CurrPoint+1 
           sta CurrentX+1 
-          sta CurrentY+1 
-          sta DeltaX
-          sta DeltaX+1 
-          sta DeltaY 
-          sta DeltaY+1 
+          sta CurrentY+1  
           lda #currXStart
           sta CurrentX
           lda #currYStart
@@ -40,6 +44,7 @@ biton:    lda $d018
  clinner:  sta (scratchPad),y // clear the bitmap 
            dey 
            bne clinner 
+           sta (scratchPad),y // clear the zeroeth byte of the page. 
            inx 
            stx scratchPad+1 
            cpx #$40 
@@ -47,7 +52,7 @@ biton:    lda $d018
 clcset:    lda #$04   //clear color RAM at $0400 - be careful near the end or BASIC memory will be corrupted
            sta scratchPad+1
            tax 
-           lda #$01 
+           lda #memColor
  clcolor:  ldy #$ff 
  clcinner: sta (scratchPad),y 
            dey
@@ -62,8 +67,13 @@ clclast:   sta (scratchPad),y
            dey 
            bne clclast
            sta (scratchPad),y // for the zeroth byte on page 7
+           lda #$00 
+           sta DeltaX
+           sta DeltaX+1 
+           sta DeltaY 
+           sta DeltaY+1
 mainLoop:  clc 
-           jsr RandHW
+           lda $d41b  // load a value from the SID's RNG - skippping the subroutine saves some time. 
            ldx #$00 
            cmp #85  
            bcc cont
@@ -118,9 +128,10 @@ mainLoop:  clc
            jmp mainLoop 
  currPntHi: inc CurrPoint+1 
            lda CurrPoint+1 
-           cmp #$08
-           beq waitSpace 
+           cmp #$10
+           beq endserp
            jmp mainLoop 
+ endserp:  getTime(time2)
  waitSpace:lda #$7F   // found in lemon64 forums
            sta $DC00 
            lda $DC01 
@@ -165,8 +176,22 @@ random: stx randx+1 //lfsr from CodeBase64
         bcc noEor
 doEor:  eor randomEor,x
 noEor:  sta seed
-// rndstr: sta rndBuffer-1
+        // now, increment the table to get histogram 
+        clc 
+        asl 
+        tax 
+        bcs rthi  // for rand values less than 128 
+        inc randTable,x 
+        bne rnxt 
+        inc randTable+1,x 
+        jmp rnxt 
+rthi:   clc      // for value greater or equal to 128 
+        inc randTable+$0100,x 
+        bne rnxt 
+        inc randTable+$0101,x 
+rnxt:   clc 
 randx:  ldx #$00  
+        lda seed
         rts 
 
 RandHW: lda $d41b     //Get a random number from the SID HW Randon Number Generator 
@@ -175,16 +200,16 @@ RandHW: lda $d41b     //Get a random number from the SID HW Randon Number Genera
 VertexX: .word 0
          .word 319
          .word 159
-VertexY: .word 0
-         .word 0
+VertexY: .word 199
          .word 199
+         .word 0
 CurrentX: .word  currXStart
 CurrentY: .word currYStart
-DeltaX: .word 0 
-DeltaY: .word 0
 seed:  .byte 78 
 CurrPoint: .word 0 
 errorPoint: .byte 0 
+time1: .dword 0 
+time2: .dword 0 
 randomEor:  .byte $1d, $2b, $2d, $4d, $5f, $63, $65, $69
             .byte $71, $87, $8d, $a9, $c3, $cf, $e7, $f5         
 
@@ -201,4 +226,17 @@ YTableHi:
     .align $100
 YTableLo:
     .fill 200, <GFX_MEM+[320*floor(i/8)]+[i&7]
+    .align $100 
+randTable: .fill 512, 0 
 
+
+
+.label swJiffyClock = $a0
+.macro getTime(outputLocation) {     
+       lda swJiffyClock  
+       sta outputLocation 
+       lda swJiffyClock+1
+       sta outputLocation+1 
+       lda swJiffyClock+2 
+       sta outputLocation+2 
+}        
